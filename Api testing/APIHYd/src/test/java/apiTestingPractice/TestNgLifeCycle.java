26eb1827d349e7e449070165dd031125f2642ad6
rajasekhar-ganji <131284665+rajasekhar-ganji@users.noreply.github.com>
2023-05-04 11:19:47 +0530

    Update TestNgLifeCycle.java

--- Api testing/APIHYd/src/test/java/apiTestingPractice/TestNgLifeCycle.java
+++ Api testing/APIHYd/src/test/java/apiTestingPractice/TestNgLifeCycle.java
@@ -12,7 +12,6 @@ public class TestNgLifeCycle extends Parentclass{
   @Test
   public void test() {
 	 
-	Response response =RestAssured.given().when().post("/create").then().log().all().extract().response();
-	System.out.println(response.getBody().asString());
+	
   }
 }
